9829a0eee40c7fe28caf1cc2c4f47fa9e866a8ed
Ryan Hechenberger <Ryan.Hechenberger@proton.me>
2024-08-04 12:16:07 +1000

    Added var parser.

---
+++ inxflow/include/inxflow/util/string.hpp
@@ -0,0 +1,81 @@
+/*
+MIT License
+
+Copyright (c) 2024 Ryan Hechenberger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef INXFLOW_UTIL_STRING_HPP
+#define INXFLOW_UTIL_STRING_HPP
+
+#include <inxlib/inx.hpp>
+
+namespace inx::flow::util
+{
+
+enum class VarClass : uint8
+{
+	Global,
+	Local,
+};
+enum class VarOp : uint8
+{
+	Name,
+	Print,
+};
+
+/**
+ * Stored result of parsed variable. Does not own the underlying string.
+ */
+struct VarName
+{
+	static constexpr size_t MaxGroupLength = 63;
+	static constexpr size_t MaxNameLength = (1 << 14) - 1;
+	const char* parsed_string_;
+	VarOp var_op_;
+	VarClass var_class_;
+	uint8 group_start_;
+	uint8 group_len_;
+	uint16 name_start_;
+	uint16 name_len_;
+
+	std::string_view group() const noexcept { return group_len_ != 0 ? std::string_view(parsed_string_ + group_start_, group_len_) : std::string_view(); }
+	std::string_view name() const noexcept { return std::string_view(parsed_string_ + name_start_, name_len_); }
+	VarOp op() const noexcept { return var_op_; }
+	VarClass cls() const noexcept { return var_class_; }
+	bool global() const noexcept { return var_class_ == VarClass::Global; }
+	bool local() const noexcept { return var_class_ == VarClass::Local; }
+	bool null() const noexcept { return parsed_string_ == nullptr; }
+
+	operator bool() const noexcept { return parsed_string_ != nullptr; }
+};
+
+/**
+ * Parse string and produce the VarName as formatted by `@[op]?[group:]?[varname]@`. `@` is optional.
+ * On success returns a filled VarName and the position parsed to. Expects to match either the whole
+ * string or if the string starts with `@`, match only until the next `@` or the whole string if missing second `@`.
+ * On invalid string, returns a null VarName and 0.
+ * @param whitespace If true, allows for whitespace in variable name. Group never permits whitespace.
+ */
+std::pair<VarName, size_t> parse_varname(std::string_view parse, bool whitespace = false);
+
+} // namespace inxflow::util
+
+#endif // INXFLOW_UTIL_STRING_HPP
---
+++ inxflow/src/util/string.cpp
@@ -0,0 +1,115 @@
+/*
+MIT License
+
+Copyright (c) 2024 Ryan Hechenberger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <inxflow/util/string.hpp>
+#include <algorithm>
+
+namespace inx::flow::util
+{
+
+std::pair<VarName, size_t> parse_varname(std::string_view parse, bool whitespace)
+{
+	using namespace std::string_view_literals;
+	// handle default case
+	switch (parse.size()) {
+	case 0:
+		return {};
+	case 1:
+		if (parse[0] == '@')
+			return {};
+		break;
+	default: // either "@@" or "x@" is an invalid parse
+		if (parse[1] == '@')
+			return {};
+		break;
+	}
+	size_t parsed_length;
+	std::string_view subparse;
+	if (parse[0] == '@') {
+		assert(parse[1] != '@');
+		if (auto p = parse.find('@', 1); p != std::string_view::npos) {
+			parsed_length = p + 1;
+			subparse = parse.substr(1, p - 1);
+		} else {
+			parsed_length = parse.size();
+			subparse = parse.substr(1, p);
+		}
+	} else {
+		parsed_length = parse.size();
+		subparse = parse;
+	}
+	assert(!subparse.empty());
+
+	// now subparse must match the whole VarName without '@' to worry about
+	VarName result{};
+	uint32 sub_at = 0;
+	if (subparse[sub_at] == '%') {
+		++sub_at;
+		result.var_op_ = VarOp::Print;
+	} else {
+		result.var_op_ = VarOp::Name;
+	}
+	// group
+	if (auto len = subparse.find(':', sub_at); len != std::string_view::npos) {
+		// found group, parse
+		len -= sub_at;
+		if (len > VarName::MaxGroupLength) {
+			// bounds check
+			return {};
+		}
+		if (len != 0) {
+			if ( std::ranges::any_of(subparse.substr(sub_at, len), [](unsigned char c) { return std::isspace(c); }) ) {
+				// no whitespace permitted in group
+				return {};
+			}
+			result.group_start_ = sub_at;
+			result.group_len_ = len;
+		}
+		sub_at += len + 1;
+	} else {
+		result.group_start_ = result.group_len_ = 0;
+	}
+	// var
+	if (sub_at >= subparse.size())
+		return {};
+	if (subparse[sub_at] == '$') {
+		++sub_at;
+		result.var_class_ = VarClass::Local;
+	} else {
+		result.var_class_ = VarClass::Global;
+	}
+	auto varname = subparse.substr(sub_at);
+	if (varname.size() == 0 || varname.size() > VarName::MaxNameLength)
+		return {}; // varname size out of bounds
+	if (varname.find_first_of(":$"sv) != std::string_view::npos)
+		return {}; // invalid character
+	if (whitespace && std::ranges::any_of(varname, [](unsigned char c) { return std::isspace(c); }))
+		return {}; // whitespace
+	result.name_start_ = sub_at;
+	result.name_len_ = varname.size();
+
+	return {result, parsed_length};
+}
+
+} // namespace inxflow::util

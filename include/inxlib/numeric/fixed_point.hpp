ff7d74a133efab8e5ac6f03f97762e1af90dbdb5
Ryan Hechenberger <Ryan.Hechenberger@proton.me>
2025-04-25 15:11:18 +1000

    Feature/fixed point (#3)
    * added fixed_point

---
+++ include/inxlib/numeric/fixed_point.hpp
@@ -0,0 +1,331 @@
+/*
+MIT License
+
+Copyright (c) 2024 Ryan Hechenberger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef INXLIB_NUMERIC_FIXED_POINT_HPP
+#define INXLIB_NUMERIC_FIXED_POINT_HPP
+
+#include <inxlib/inx.hpp>
+#include <inxlib/util/bits.hpp>
+#include "int128.hpp"
+
+namespace inx::numeric
+{
+
+template <size_t Digits>
+struct binary_fixed_width_deduce
+{
+	using type = std::int64_t;
+	constexpr size_t digits = 64;
+	constexpr bool overflow = true;
+};
+// fit into i64
+template <size_t Digits>
+requires (32 > Digits && Digits <= 64)
+struct binary_fixed_width_deduce<Digits>
+{
+	using type = std::int64_t;
+	constexpr size_t digits = Digits;
+	constexpr bool overflow = false;
+};
+// fit into i32
+template <size_t Digits>
+requires (16 > Digits && Digits <= 32)
+struct binary_fixed_width_deduce<Digits>
+{
+	using type = std::int32_t;
+	constexpr size_t digits = Digits;
+	constexpr bool overflow = false;
+};
+// fit into i16
+template <size_t Digits>
+requires (8 > Digits && Digits <= 16)
+struct binary_fixed_width_deduce<Digits>
+{
+	using type = std::int16_t;
+	constexpr size_t digits = Digits;
+	constexpr bool overflow = false;
+};
+// fit into i8
+template <size_t Digits>
+requires (Digits <= 8)
+struct binary_fixed_width_deduce<Digits>
+{
+	using type = std::int8_t;
+	constexpr size_t digits = Digits;
+	constexpr bool overflow = false;
+};
+
+template <size_t Digits, size_t Fraction>
+struct binary_fixed_point
+{
+	using deduce = binary_fixed_width_deduce<Digits>;
+	using value_type = typename deduce::type;
+	static_assert(Digits <= deduce::digits, "Digits must not exceed max integer size.");
+	static_assert(Fraction < deduce::digits, "Fraction must be less than integer size.");
+
+	consteval static size_t digits() noexcept { return deduce::digits; }
+	consteval static size_t logical_digits() noexcept { return Digits; }
+	consteval static size_t frac() noexcept { return Fraction; }
+	consteval static bool overflow() noexcept { return deduce::overflow; }
+
+	value_type value;
+	
+	binary_fixed_point() noexcept = default;
+	constexpr explicit binary_fixed_point(value_type v) noexcept : value{v}
+	{ }
+	constexpr binary_fixed_point(binary_fixed_point v) noexcept = default;
+	constexpr binary_fixed_point(binary_fixed_point&& v) noexcept = default;
+	template <std::floating_point FP>
+	explicit binary_fixed_point(FP val) noexcept :
+		value(static_cast<FP>(val) * (1 << Fraction))
+	{ }
+	~binary_fixed_point() = default;
+
+	template <std::floating_point FP>
+	explicit operator FP() const noexcept
+	{
+		return static_cast<FP>(value) / (1 << Fraction);
+	}
+	template <std::integral Int>
+	constexpr explicit operator Int() const noexcept
+	{
+		return static_cast<int>(value >> Fraction);
+	}
+	template <size_t OD, size_t OF>
+	constexpr explicit(OD < Digits) operator binary_fixed_point<OD, OF>() const noexcept
+	{
+		using to_type = binary_fixed_point<OD, OF>;
+		using bit_type = std::conditional_t<(Digits >= OD), value_type, typename to_type::value_type>;
+		return to_type(static_cast<typename to_type::value_type>(
+			inx::util::bit_shift(static_cast<bit_type>(value), OF - Fraction)));
+	}
+};
+
+template <typename T>
+concept BinaryFixedPoint = std::same_as<T, binary_fixed_point<T::digits(), T::frac()>>;
+
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+using common_binary_fixed_point = binary_fixed_point<
+	std::max(A::logical_digits(), B::logical_digits()) + ( std::max(A::frac(), B::frac()) - std::min(A::frac(), B::frac()) ),
+	std::max(A::frac(), B::frac()) >;
+
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+using common_binary_fixed_point = binary_fixed_point<
+	std::max(A::logical_digits(), B::logical_digits()) + ( std::max(A::frac(), B::frac()) - std::min(A::frac(), B::frac()) ),
+	std::max(A::frac(), B::frac()) >;
+
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+struct multiply_binary_fixed_point
+{
+	using type = binary_fixed_point<A::logical_digits() + B::logical_digits(), A::frac() + B::frac()>;
+	constexpr static bool fits_int = true;
+};
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+	requires (A::logical_digits() + B::logical_digits() > 64)
+struct multiply_binary_fixed_point<A, B>
+{
+	constexpr static size_t frac_total = A::frac() + B::frac();
+	constexpr static size_t frac_max = std::max(A::frac(), B::frac());
+	using type = binary_fixed_point<A::logical_digits() + B::logical_digits(), A::frac() + B::frac()>;
+	constexpr static bool fits_int = false;
+};
+
+/**
+ * @return plus operator of type common_binary_fixed_point<A,B>
+ */
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+constexpr auto operator+(A a, B b) noexcept
+{
+	if (std::same_as<A, B>) {
+		return A(a.value + b.value);
+	} else {
+		using type = common_binary_fixed_point<A,B>;
+		using vt = typename type::value_type;
+		return type(static_cast<vt>( static_cast<type>(a).value + static_cast<type>(b).value ));
+	}
+}
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+constexpr A& operator+=(A& a, B b) noexcept
+{
+	a.value += static_cast<A>(b).value;
+	return a;
+}
+
+/**
+ * @return plus operator of type common_binary_fixed_point<A,B>
+ */
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+constexpr auto operator-(A a, B b) noexcept
+{
+	if (std::same_as<A, B>) {
+		return A(a.value - b.value);
+	} else {
+		using type = common_binary_fixed_point<A,B>;
+		using vt = typename type::value_type;
+		return type(static_cast<vt>( static_cast<type>(a).value - static_cast<type>(b).value ));
+	}
+}
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+constexpr A& operator-=(A& a, B b) noexcept
+{
+	a.value -= static_cast<A>(b).value;
+	return a;
+}
+
+/**
+ * @return plus operator of type common_binary_fixed_point<A,B>
+ */
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+constexpr auto operator*(A a, B b) noexcept
+{
+	using multi = multiply_binary_fixed_point<A, B>;
+	if constexpr (multi::fits_int) {
+		int64_t res = static_cast<int64_t>(a.value) * static_cast<int64_t>(b.value);
+		return multi::type(static_cast<multi::type::value_type>(res));
+	} else {
+#ifdef INX_INT128
+		int128 res = static_cast<int128>(a.value) * b.value;
+		res >>= A::frac() + B::frac() - multi::type::frac();
+		return multi::type(static_cast<multi::type::value_type>(res));
+#else
+		assert(false);
+		return multi::type{};
+#endif
+	}
+}
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+constexpr A& operator*=(A& a, B b) noexcept
+{
+	using multi = multiply_binary_fixed_point<A, B>;
+	if constexpr (multi::fits_int) {
+		int64_t res = static_cast<int64_t>(a.value) * static_cast<int64_t>(b.value);
+		a.value = static_cast<A::value_type>( res >> B::frac() );
+	} else {
+#ifdef INX_INT128
+		int128 res = static_cast<int128>(a.value) * b.value;
+		a.value = static_cast<A::value_type>( res >> B::frac() );
+#else
+		assert(false);
+		a.value = 0;
+#endif
+	}
+	return *this;
+}
+
+template <BinaryFixedPoint A>
+constexpr A operator>>(A a, uint32_t b) noexcept
+{
+	assert(b < sizeof(A::value_type) * CHAR_BIT);
+	return A(static_cast<A::value_type>(a.value >> b));
+}
+template <BinaryFixedPoint A>
+constexpr A& operator>>=(A& a, uint32_t b) noexcept
+{
+	assert(b < sizeof(A::value_type) * CHAR_BIT);
+	A.value >>= b;
+	return a;
+}
+template <BinaryFixedPoint A>
+constexpr A operator<<(A a, uint32_t b) noexcept
+{
+	assert(b < sizeof(A::value_type) * CHAR_BIT);
+	return A(static_cast<A::value_type>(a.value << b));
+}
+template <BinaryFixedPoint A>
+constexpr A& operator<<=(A& a, uint32_t b) noexcept
+{
+	assert(b < sizeof(A::value_type) * CHAR_BIT);
+	A.value <<= b;
+	return a;
+}
+
+template <BinaryFixedPoint A>
+constexpr A operator|(A a, A b) noexcept
+{
+	return A( static_cast<A::value_type>(a.value | b.value) );
+}
+template <BinaryFixedPoint A>
+constexpr A& operator|=(A& a, A b) noexcept
+{
+	a.value |= b.value;
+	return a;
+}
+template <BinaryFixedPoint A>
+constexpr A operator&(A a, A b) noexcept
+{
+	return A( static_cast<A::value_type>(a.value & b.value) );
+}
+template <BinaryFixedPoint A>
+constexpr A& operator&=(A& a, A b) noexcept
+{
+	a.value &= b.value;
+	return a;
+}
+template <BinaryFixedPoint A>
+constexpr A operator^(A a, A b) noexcept
+{
+	return A( static_cast<A::value_type>(a.value ^ b.value) );
+}
+template <BinaryFixedPoint A>
+constexpr A& operator^=(A& a, A b) noexcept
+{
+	a.value ^= b.value;
+	return a;
+}
+
+template <BinaryFixedPoint A>
+constexpr std::strong_ordering operator<=>(A a, A b) noexcept
+{
+	return a.value <=> b.value;
+}
+template <BinaryFixedPoint A, BinaryFixedPoint B>
+	requires (!std::same_as<A,B>)
+constexpr std::strong_ordering operator<=>(A a, B b) noexcept
+{
+	using common = common_binary_fixed_point<A, B>;
+	if constexpr (A::frac() == B::frac() || common::logical_digits() <= 64) {
+		return static_cast<common>(a) <=> static_cast<common>(b);
+	} else {
+		// overflow, handle
+#ifdef INX_INT128
+		int128 a128 = static_cast<int128>(a.value);
+		int128 b128 = static_cast<int128>(b.value);
+		// align
+		if constexpr (A::frac() > B::frac()) {
+			b128 <<= (A::frac() - B::frac());
+		} else {
+			a128 <<= (B::frac() - A::frac());
+		}
+		return a128 <=> b128;
+#else
+		// handle
+		assert(false);
+		return static_cast<common>(a) <=> static_cast<common>(b);
+#endif
+	}
+}
+
+} // namespace inx::numeric
+
+#endif // INXLIB_NUMERIC_FIXED_POINT_HPP
---
+++ include/inxlib/numeric/int128.hpp
@@ -0,0 +1,76 @@
+/*
+MIT License
+
+Copyright (c) 2024 Ryan Hechenberger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef INXLIB_NUMERIC_INT128_HPP
+#define INXLIB_NUMERIC_INT128_HPP
+
+#include <inxlib/inx.hpp>
+
+namespace inx::numeric
+{
+
+#ifdef __SIZEOF_INT128__
+#define INX_INT128
+using int128 = __int128;
+// using uint128 = unsigned __int128;
+#else
+#if 0
+struct int128
+{
+	uint64_t part1;
+	int64_t part2;
+	constexpr int128& operator=(const int128&) noexcept = default;
+	template <std::signed_integral T>
+	constexpr int128& operator=(T v) noexcept
+	{
+		part1 = static_cast<uint64_t>(static_cast<int64_t>(v));
+		part2 = v >= 0 ? 0ll : ~0ll;
+	}
+	template <std::unsigned_integral T>
+	constexpr int128& operator=(T v) noexcept
+	{
+		part1 = static_cast<uint64_t>(v);
+		part2 = 0;
+	}
+
+	template <std::integral T>
+	constexpr operator T() const noexcept
+	{
+		return static_cast<T>(parts[0]);
+	}
+};
+
+constexpr int128& operator+=(int128& lhs, int128 rhs) noexcept
+{
+	int64_t overflow = lhs.parts[0] + rhs.parts[0];
+}
+
+int128& operator>>=(size_t v);
+#else
+using int128 = int64_t;
+#endif
+
+} // namespace inx::numeric
+
+#endif // INXLIB_NUMERIC_INT128_HPP

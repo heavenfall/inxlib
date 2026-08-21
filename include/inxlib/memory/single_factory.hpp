ccfe019978511dee423341c1fe3717c959b734db
Ryan Hechenberger <Ryan.Hechenberger@proton.me>
2026-08-21 13:21:50 +1000

    update license year

--- include/inxlib/memory/single_factory.hpp
+++ include/inxlib/memory/single_factory.hpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2025 Ryan Hechenberger
+Copyright (c) 2026 Ryan Hechenberger
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
